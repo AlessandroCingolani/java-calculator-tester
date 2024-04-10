ee2b87c0061a5ebbd8cebb003039d069cfe5e2af
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-10 14:19:35 +0200

    Add all methods

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,24 @@
 package calculator;
 
 public class Calculator {
+
+    private Calculator() {
+    }
+
+    // methods
+    public float add(float num1, float num2){
+        return num1 + num2;
+
+    }  public float subtract(float num1, float num2){
+        return num1 - num2;
+    }
+    public float divide(float num1, float num2){
+        if(num1 == 0 || num2 == 0){
+            throw new IllegalArgumentException("Cannot divide with 0");
+        }
+        return num1 / num2;
+    }
+    public float multiply(float num1, float num2){
+        return num1 * num2;
+    }
 }
