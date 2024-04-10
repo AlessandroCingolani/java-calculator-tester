1287a42600c90d06cdb95eaa2c5d676902020485
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-10 16:03:50 +0200

    Add TicketTest test costruttore

--- src/test/java/ticket/TicketTest.java
+++ src/test/java/ticket/TicketTest.java
@@ -9,6 +9,15 @@ class TicketTest {
 
 
     @Test
+    void testCostruttore(){
+        assertThrows(IllegalArgumentException.class,() -> new Ticket (-1,100));
+        assertThrows(IllegalArgumentException.class,() -> new Ticket (10,-1));
+        assertThrows(IllegalArgumentException.class,() -> new Ticket (0,100));
+        assertDoesNotThrow(() ->new Ticket(20,10));
+    }
+
+
+    @Test
     void getPriceTicket() {
         Ticket ticket1 = new Ticket(12,100);
         assertEquals(16.8, ticket1.getPriceTicket());
